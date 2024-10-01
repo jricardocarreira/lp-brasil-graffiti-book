400f206e661b8114176980c1813be96160a993d5
Jose Ricardo Carreira <jricardocarreira@gmail.com>
2024-10-01 06:12:16 -0300

    feat: add font link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,13 +23,16 @@ export default function Home() {
           content="Conheça o Brasil Graffiti Book, o livro que traz imagens autorais das ruas de São Paulo, feito para entusiastas, artistas amadores e profissionais!"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <Container maxWidth="md">
-          <HeroSection plansRef={plansRef}/>
+          <HeroSection plansRef={plansRef} />
           <KnowAbout />
-          <FormSubmit plansRef={plansRef}/>
+          <FormSubmit plansRef={plansRef} />
           <AboutMe />
           <Questions />
         </Container>
